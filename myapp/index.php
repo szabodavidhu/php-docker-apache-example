72104f90d32d5ea551fdd41c34a01c29a748be01
David Szabo <szabo.david.hu@gmail.com>
2017-12-14 08:50:38 +0100

    Update index.php

--- myapp/index.php
+++ myapp/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <p>
-            Hello. Today is <?= date('l \t\h\e jS') ?>.
+            Hello v2. Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
 </html>
